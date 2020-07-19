a069ce32901efba4bb6ce0978cae1bfbb9670d29
Jgunha <jgunha@gmail.com>
2020-07-20 03:09:33 +0900

    get text of pdf file (not complete)

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-pdf DLP
+#pdf DLP
+import PyPDF2
+import os
+
+file = open("test1.pdf", 'rb')
+fileReader = PyPDF2.PdfFileReader(file)
+numberPages = fileReader.getNumPages()
+print(fileReader.numPages)
+pageObj = fileReader.getPage(0)
+text = pageObj.extractText()
+print(text)
+#주민번호 정규표현식
+#^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$
