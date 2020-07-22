e87bc5bb21e641536ca055a4101317f4c534bee1
Jgunha <jgunha@gmail.com>
2020-07-22 23:51:05 +0900

    find RRN of pdf file

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-#pdf DLP
-import PyPDF2
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
 import os
+import re
+import sys
 
-file = open("test1.pdf", 'rb')
-fileReader = PyPDF2.PdfFileReader(file)
-numberPages = fileReader.getNumPages()
-print(fileReader.numPages)
-pageObj = fileReader.getPage(0)
-text = pageObj.extractText()
-print(text)
-#주민번호 정규표현식
-#^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$
+p = re.compile('(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}')
+
+
+def convert_pdf_to_txt(path):
+    rsrcmgr = PDFResourceManager()
+    retstr = StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    fp = open(path, 'rb')
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    password = ""
+    maxpages = 0
+    caching = True
+    pagenos = set()
+
+    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages,
+                                  password=password,
+                                  caching=caching,
+                                  check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    fp.close()
+    device.close()
+    retstr.close()
+    return text
+
+try:
+    text = convert_pdf_to_txt(os.getcwd() +'\\'+ sys.argv[1])
+    texts = text.split()
+    for text in texts:
+        m = p.search(text)
+        if m is not None:
+            print(m.group())
+except:
+    print('wrong usage....\nUSAGE -> "python3 main.py [pdf_file]"')
